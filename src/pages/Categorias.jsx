43c2a585be5aa2038bd50aaa90e9869f4558c65e
andres arriaga <andresfelipearriaga@gmail.com>
2024-02-06 23:03:48 -0500

    responsive en carrito de compras y categoria

--- src/pages/Categorias.jsx
+++ src/pages/Categorias.jsx
@@ -4,8 +4,8 @@ import Data from "../data/categorias.json";
 const Categorias = () => {
   return (
     <>
-      <section className=" flex justify-center items-center h-screen w-full">
-        <main className="max-w-[1500px] flex gap-5 flex-wrap justify-center">
+      <section className=" flex justify-center items-center h-screen w-full flex-wrap ">
+        <main className="max-w-[1500px] flex gap-5 flex-wrap justify-center mt-20">
           {Data.map((el) => {
             return (
               <Cards key={el.categoria} name={el.categoria} img={el.img} />
--- src/pages/Tu-carrito.jsx
+++ src/pages/Tu-carrito.jsx
@@ -2,8 +2,8 @@ const TuCarrito = ({logueado}) => {
 
   return (
     <>
-      <section className="mt-20 w-full flex justify-center items-center ">
-        <div className="w-full h-96 rounded-lg bg-slate-100 max-w-[1500px] flex justify-center items-center p-5">
+      <section className="mt-20 w-full flex flex-col md:flex-row justify-center items-center ">
+        <div className="w-full h-96 rounded-lg bg-slate-100 max-w-[1500px] flex flex-col md:flex-row justify-center items-center p-5">
           <img
             className="w-80"
             src="https://i.ibb.co/nkh2VK4/icono-carrito-compras-estilo-comic-ilustracion-vector-dibujos-animados-carro-sobre-fondo-blanco-aisl.png"
