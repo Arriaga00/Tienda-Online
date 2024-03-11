ba52c1b3a6ca63ab1de33231ed89ec39e3d3b56f
andres arriaga <andresfelipearriaga@gmail.com>
2024-03-11 15:43:06 -0500

    organizando articulo de inicio

--- src/components/CardContenido.jsx
+++ src/components/CardContenido.jsx
@@ -1,28 +1,37 @@
 /* eslint-disable react/prop-types */
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const CardContenido = ({descripcion,precio,img,titulo}) => {
+const CardContenido = ({ descripcion, precio, img, titulo }) => {
   return (
     <>
-    <Link to={`/${(titulo).replace(/\s/g, '-')}`} className="group w-80 border pb-5 hover:scale-105 ease-in-out transition-all">
-        <img
+      <article
+        className="group w-80 border pb-5 hover:scale-105 ease-in-out transition-all"
+      >
+        <Link to={`/${titulo.replace(/\s/g, "-")}`}>
+          <img
             alt="producto"
             src={img}
             className="h-56 w-full rounded-xl object-contain object-center shadow-xl transition "
-        />
+          />
 
-        <div className="p-4">
-            <h3 className="text-lg font-medium text-gray-900 text-start">{descripcion}</h3>
+          <div className="p-4">
+            <h3 className="text-lg font-medium text-gray-900 text-start">
+              {descripcion}
+            </h3>
 
             <p className="mt-2 line-clamp-3 text-lg/relaxed text-green-500 font-bold">
-            {precio+"$"}
+              {precio + "$"}
             </p>
-        </div>
-        <button className="rounded-full bg-yellow-500 font-semibold px-8 py-3 ">Añadir al carrito</button>
-    </Link>
-    </>
-  )
-}
+          </div>
+        </Link>
 
-export default CardContenido
+        <button className="rounded-full bg-yellow-500 font-semibold px-8 py-3 ">
+          Añadir al carrito
+        </button>
+      </article>
+    </>
+  );
+};
+
+export default CardContenido;
